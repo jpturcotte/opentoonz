9c0c85758956d27ee535725bb0475676ae4b423e
Jean-Philippe Turcotte <jp2k5@hotmail.com>
2019-10-15 12:46:59 -0400

    Upgrade Travis' Linux environment to Ubuntu 18.04 LTS
    Also, enable ccache for Clang builds, and update libmypaint 1.4.0.

--- ci-scripts/linux/travis-install.sh
+++ ci-scripts/linux/travis-install.sh
@@ -1,9 +1,4 @@
-sudo add-apt-repository --yes ppa:beineri/opt-qt597-trusty
-sudo add-apt-repository --yes ppa:achadwick/mypaint-testing
+sudo add-apt-repository --yes ppa:beineri/opt-qt-5.12.3-bionic
+sudo add-apt-repository --yes ppa:otto-kesselgulasch/gimp-edge
 sudo apt-get update
-sudo apt-get install -y liblzo2-dev liblz4-dev libfreetype6-dev libpng-dev libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libglew-dev freeglut3-dev qt59script libsuperlu3-dev qt59svg qt59tools qt59multimedia wget libusb-1.0-0-dev libboost-all-dev liblzma-dev libjson-c-dev libmypaint-dev
-
-# someone forgot to include liblz4.pc with the package, use the version from xenial, as it only depends on libc
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/lz4/liblz4-1_0.0~r131-2ubuntu2_amd64.deb -O liblz4.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/lz4/liblz4-dev_0.0~r131-2ubuntu2_amd64.deb -O liblz4-dev.deb
-sudo dpkg -i liblz4.deb liblz4-dev.deb
+sudo apt-get install -y liblzo2-dev liblz4-dev libfreetype6-dev libpng-dev libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libglew-dev freeglut3-dev qt512script qt512svg qt512tools qt512multimedia libusb-1.0-0-dev libboost-all-dev liblzma-dev libjson-c-dev libmypaint-dev libsuperlu-dev
